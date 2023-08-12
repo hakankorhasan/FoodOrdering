f3bf3ed74070b30fbaaa3508d722b86f72ee2d28
Hakan Körhasan <hakankorhasann@gmail.com>
2023-08-12 14:11:18 +0300

    List Dishes in category

---
+++ CustomViews/TableViewCells/DishListTableViewCells/DishListTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  DishListTableViewCell.swift
+//  FoodOrdering
+//
+//  Created by Hakan Körhasan on 12.08.2023.
+//
+
+import UIKit
+import Kingfisher
+
+class DishListTableViewCell: UITableViewCell {
+
+    static let identifier = String(describing: DishListTableViewCell.self)
+    
+    @IBOutlet weak var dishImageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    
+    func setup(dish: Dish) {
+        dishImageView.kf.setImage(with: dish.image?.asUrl)
+        titleLabel.text = dish.title
+        descriptionLabel.text = dish.description
+    }
+    
+}
---
+++ Views/ListDishes/ListDishesViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ListDishesViewController.swift
+//  FoodOrdering
+//
+//  Created by Hakan Körhasan on 12.08.2023.
+//
+
+import UIKit
+
+class ListDishesViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var category: DishCategory!
+    
+    var dishList: [Dish] = [
+        .init(id: "id1", title: "Hakan", description: "Lorem ipsum dolor sit amet", image: "https://picsum.photos/100/200", calories: 34),
+        .init(id: "id1", title: "Birsu", description: "Bursanın en güzel, gözde yemeklerin mekanı...", image: "https://picsum.photos/100/200", calories: 340),
+        .init(id: "id1", title: "Ahmet", description: "Bursanın en güzel, gözde yemeklerin mekanı...", image: "https://picsum.photos/100/200", calories: 150),
+        .init(id: "id1", title: "Enes", description: "Bursanın en güzel, gözde yemeklerin mekanı...", image: "https://picsum.photos/100/200", calories: 180),
+        .init(id: "id1", title: "Burak", description: "Bursanın en güzel, gözde yemeklerin mekanı...", image: "https://picsum.photos/100/200", calories: 600),
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = category.name
+        registerCells()
+    }
+    
+    private func registerCells() {
+        tableView.register(UINib(nibName: DishListTableViewCell.identifier, bundle: nil), forCellReuseIdentifier: DishListTableViewCell.identifier)
+    }
+}
+
+extension ListDishesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        dishList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: DishListTableViewCell.identifier, for: indexPath) as! DishListTableViewCell
+        
+        cell.setup(dish: dishList[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = DishDetailViewController.instantiate()
+        controller.dish = dishList[indexPath.row]
+      //  controller.modalPresentationStyle = .fullScreen
+      //  controller.modalTransitionStyle = .coverVertical
+        navigationController?.present(controller, animated: true)
+    }
+}
